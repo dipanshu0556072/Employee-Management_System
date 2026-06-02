f9819758b95b9c77c184be65e531f0fdd6efc283
dipanshu0556072 <dipanshukumar20aug@gmail.com>
2026-06-02 15:05:25 +0530

    delete the microservice

---
+++ employee_info/src/main/java/com/example/employee_info/EmployeeInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeInfoApplication.class, args);
+	}
+
+}
